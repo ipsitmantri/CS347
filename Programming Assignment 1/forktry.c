3c5ef1f4f40f3f38f6925e883d652c97aab1abbb
Ipsit <mmkipsit@gmail.com>
2022-01-12 14:53:50 +0530

    PartA_1_2_3

---
+++ Programming Assignment 1/forktry.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+void main(int argc, void* argv) {
+
+	for (int i = 0; i < 4; i++) {
+		int ret = fork();
+		if (ret == 0) {
+			printf("child %d\n", i);
+		}
+		else {
+			printf("else block %d\n", i);
+		}
+	}
+
+}
